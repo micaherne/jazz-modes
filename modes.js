5be8c7e63354892f381749249bec1ebe258ad8c5
Michael Aherne <micaherne@blueyonder.co.uk>
2013-11-10 20:50:18 +0000

    Initial import.

---
+++ modes.js
@@ -0,0 +1,17 @@
+var Modes = {
+		
+	"arse": "nim",
+	
+	"scaleTypes": {
+		"major": [2, 2, 1, 2, 2, 2, 1]
+	},
+		
+	"init": function() {
+		this.arse = "Hello!";
+	},
+	
+	"randomMode": function() {
+		
+	}
+	
+};
